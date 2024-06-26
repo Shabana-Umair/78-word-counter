a9a1870b72548f0ab4162fe67058d3221d1fd3e3
Shabana-Umair <163396087+Shabana-Umair@users.noreply.github.com>
2024-06-26 01:01:17 -0700

    word counter

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node     
+// this is shebang,it tells the os to run it with nodejs
+// import inquirer from inquirer module which is a commond line interface from node.js
+import inquirer from "inquirer";
+// declare a const answer and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }]);
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to console
+// console.log(words);  shows words in an array
+// print the word count of sentencs
+console.log(`Your word count is ${words.length}`);
